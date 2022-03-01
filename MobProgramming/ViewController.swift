0d97fc0b84059b32add0b89809e7d1d17a5fe75b
Martin List <mlist@eos-uptrade.de>
2022-03-01 15:24:16 +0100

    Adds Presenter

--- MobProgramming/ViewController.swift
+++ MobProgramming/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UITableViewController {
+    private let presenter: Presenter
+
+    init(presenter: Presenter) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        presenter.numberOfSections
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        presenter.numberOfRows
+    }
 }
 
