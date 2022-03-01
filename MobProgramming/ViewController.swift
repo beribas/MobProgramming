446e98e5544e8af5d94a1400396baf0f52f21608
Oleg Langer <olanger.ext@eos-uptrade.de>
2022-03-01 10:16:06 +0100

    Add xcode project

---
+++ MobProgramming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobProgramming
+//
+//  Created by Oleg Langer on 01.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
